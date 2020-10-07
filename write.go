e92b854b7b8082b7a87ed7cf9345606f2a0a1516
ksmiti <kilian.smiti@outlook.fr>
2020-10-07 15:44:47 +0200

    feat: add prompt

--- write.go
+++ write.go
@@ -1,6 +1,11 @@
 package cli
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 const (
 	InfoColor    = "\033[1;34m"
@@ -12,7 +17,26 @@ const (
 	ResetColor   = "\033[0;0m"
 )
 
-func Write (msg string, args... interface{}) {
+func Write(msg string, args... interface{}) {
 	fmt.Printf(msg + "\n", args...)
 }
 
+func Prompt(defaultChoice bool, msg string, args... interface{}) bool {
+	var dcText string
+	if defaultChoice {
+		dcText = "[Y/n]"
+	} else {
+		dcText = "[y/N]"
+	}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf(msg + " " + dcText + "\n", args...)
+	str, _ := reader.ReadString('\n')
+	switch strings.ToLower(strings.TrimSpace(str)) {
+	case "y", "Y":
+		return true
+	case "n", "N":
+		return false
+	default:
+		return defaultChoice
+	}
+}
